e77e7166de8a9e86552cb65e570a3d99b726665c
allen hsu <zzsh10011725@gmail.com>
2024-12-22 16:04:53 +0800

    更改p

--- products.py
+++ products.py
@@ -1,15 +1,13 @@
-products_name = []
-products_price = []
+products = []
+
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
 		break
-	products_name.append(name)
-
 	price = input('清輸入價格:')
-	if price == 'q':
-		break
-	products_price.append(price)
+	products.append([name, price])
 
-print(products_name, products_price)
+print(products)
+
+# print(products_name, products_price)
 # print(products_price)
